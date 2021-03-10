f41825b24da0ca19d0dfa20edf2a0fa7c47c0c44
brightestsirius <katherina.shypovskaya@gmail.com>
2021-03-10 14:16:24 +0200

    first project commit

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+let gulp = require('gulp');
+
+gulp.task('html',function(resolve){
+	gulp
+		.src(['src/index.html'])
+		.pipe(gulp.dest('build/'));
+	resolve();
+})
+
+gulp.task('images',function(done){
+	gulp
+		.src('src/images/*')
+		.pipe(gulp.dest('build/images'));
+	done();
+})
+
+gulp.task('css',function(done){
+	gulp
+		.src('src/css/style.css')
+		.pipe(gulp.dest('build/css'));
+	done();
+})
+
+gulp.task('js',function(done){
+	gulp
+		.src('src/js/*.js')
+		.pipe(gulp.dest('build/js'));
+	done();
+})
+
+gulp.task('all', gulp.series('html','images','css','js'));
